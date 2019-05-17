e47dfaf23c6c6d047886b9fd2a249ef2de28f1e9
Henry <595551242@qq.com>
2019-05-17 09:49:03 +0800

    update

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,4 @@
 module.exports = {
   NODE_ENV: '"production"',
-  // BASE_API: '"http://58.246.226.254:8089/v1"'
-  BASE_API: '"http://192.168.0.7/v1"'
+  BASE_API: '"http://58.246.226.254:8089/v1"'
 }
